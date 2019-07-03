8f1e6dadedef05390b2e0f1e9107c55b50baa8ec
admin <admin@gov.com>
2019-07-03 10:47:57 +0800

    添加可调速动画

--- js/main.js
+++ js/main.js
@@ -1,25 +1,45 @@
 ! function () {
+  var duration = 50
   function writeCode(prefix, code, fn) {
     let container = document.querySelector('#code')
     let styleTag = document.querySelector('#styleTag')
     let n = 0
-    let id = setInterval(() => {
+    let id = setTimeout(function run(){
       n += 1
       container.innerHTML = code.substring(0, n)
       styleTag.innerHTML = code.substring(0, n)
       container.scrollTop = container.scrollHeight
-      if (n >= code.length) {
-        window.clearInterval(id)
+      if (n < code.length) {
+        id = setTimeout(run, duration)
+      }else{
         fn && fn.call()
       }
-    }, 1)
+    }, duration)
   }
+
+  $('.actions').on('click', 'button', function(e){
+    let $button = $(e.currentTarget)
+    let speed = $button.attr('data-speed')
+    $button.addClass('active')
+      .siblings('.active').removeClass('active')
+    switch(speed){
+      case 'slow':
+        duration = 50
+        break
+      case 'normal':
+        duration = 30
+        break
+      case 'fast':
+        duration = 5
+        break
+    }
+  })
+
   let code = `/*
   *先准备皮卡丘的脸
   */
   .preview {
     height: 100%;
-    border: 1px blue solid;
     display: flex;
     justify-content: center;
     align-items: center;
